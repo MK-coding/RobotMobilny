6e0cc927a49cfeb74e1472847c81f98f9dfb601d
MK-coding <81474503+MK-coding@users.noreply.github.com>
2021-05-31 15:11:14 +0200

    Add files via upload
    First Uploaded Version

---
+++ RobotMobilny/RobotMobilny/Engines.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+
+namespace RobotMobilny
+{
+    class Engines
+    {
+        
+        private int valueLeft=0 , valueRight=0;
+        private bool isRunning;
+        private Change change;
+        private int power;
+        public void startEngine()
+        {
+            if (!isRunning)
+            {
+                return;
+            }
+         
+            if (change.Equals(Change.INCREASE)){
+                switch(power)
+                {
+                    case 0:
+                        valueLeft = 30;
+                        valueRight = 30;
+                        break;
+                    case 1:
+                        valueLeft = 60;
+                        valueRight = 60;
+                        break;
+                    case 2:
+                        valueLeft = 80;
+                        valueRight = 80;
+                        break;
+                    case 3:
+                        valueLeft = 100;
+                        valueRight = 100;
+                        break;
+
+                }
+            }
+            if (change.Equals(Change.DECREASE))
+            {
+                switch (power)
+                {
+                    case 0:
+                        valueLeft = -30;
+                        valueRight = -30;
+                        break;
+                    case 1:
+                        valueLeft = -60;
+                        valueRight = -60;
+                        break;
+                    case 2:
+                        valueLeft = -80;
+                        valueRight = -80;
+                        break;
+                    case 3:
+                        valueLeft = -100;
+                        valueRight = -100;
+                        break;
+
+                }
+            }
+            if (change.Equals(Change.TURNLEFT))
+            {
+                
+
+                switch (power)
+                {
+                    case 0:
+                        valueLeft = -15;
+                        valueRight = 15;
+                        break;
+                    case 1:
+                        valueLeft = -30;
+                        valueRight = 30;
+                        break;
+                    case 2:
+                        valueLeft = -40;
+                        valueRight = 40;
+                        break;
+                    case 3:
+                        valueLeft = -50;
+                        valueRight = 50;
+                        break;
+                }
+            }
+            if (change.Equals(Change.TURNRIGHT))
+            {
+                switch (power)
+                {
+                    case 0:
+                        valueLeft = 15;
+                        valueRight = -15;
+                        break;
+                    case 1:
+                        valueLeft = 30;
+                        valueRight = -30;
+                        break;
+                    case 2:
+                        valueLeft = 40;
+                        valueRight = -40;
+                        break;
+                    case 3:
+                        valueLeft = 50;
+                        valueRight = -50;
+                        break;
+                }
+            }
+            if (change.Equals(Change.STAY))
+            {
+                valueLeft = 0;
+                valueRight = 0;
+            }
+
+        }
+       
+        public void workForward(int power)
+         {
+            isRunning = true;
+            this.power = power;
+            change = Change.INCREASE;
+            
+         }
+        public void workBackward(int power)
+        {
+            isRunning = true;
+            this.power = power;
+            change = Change.DECREASE;
+        }
+        public void workLeft(int power)
+        {
+            isRunning = true;
+            this.power = power;
+            change = Change.TURNLEFT;
+           
+        }
+        public void workRight(int power)
+        {
+            isRunning = true;
+            this.power = power;
+            change = Change.TURNRIGHT;
+        }
+        public void noPower()
+        {
+            isRunning = true;
+            change = Change.STAY;
+        }
+
+        public int getLeftValue()
+        {
+            return valueLeft;
+        }
+        public int getRightValue()
+        {
+            return valueRight;
+        }
+        
+
+    }
+        enum Change
+        {
+            INCREASE, DECREASE , STAY , TURNLEFT , TURNRIGHT 
+        }
+
+}
---
+++ RobotMobilny/RobotMobilny/Frame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotMobilny
+{
+    class Frame
+    {
+        public static string[] EncodeSensors(string readdata)
+        {
+            string a, b, c, d, e, f, g, h, i, j;
+
+            a = readdata.Substring(7, 2);
+            b = readdata.Substring(9, 2);
+            c = readdata.Substring(11, 2);
+            d = readdata.Substring(13, 2);
+            e = readdata.Substring(15, 2);
+            f = readdata.Substring(17, 2);
+            g = readdata.Substring(19, 2);
+            h = readdata.Substring(21, 2);
+            i = readdata.Substring(23, 2);
+            j = readdata.Substring(25, 2);
+
+            string s1 = b + a;
+            string s2 = d + c;
+            string s3 = f + e;
+            string s4 = h + g;
+            string s5 = j + i;
+
+            string[] sensorsValue = new string[] { s1, s2, s3, s4, s5 };
+
+            return sensorsValue;
+        }
+
+        
+    }
+}
